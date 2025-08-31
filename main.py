ab74072e1ebf476397b1e1c15b23479d11bcc09c
TrueFacts <verytruefacts10@gmail.com>
2025-08-31 01:25:50 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+Should look something like this:
+_____________________
+| 1 | 2 | 3 | 4 | 5 | # Guess 1
+‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+_____________________
+| 1 | 2 | 3 | 4 | 5 | # Guess 2
+‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+_____________________
+| 1 | 2 | 3 | 4 | 5 | # Guess 3
+‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+_____________________
+| 1 | 2 | 3 | 4 | 5 | # Guess 4
+‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+_____________________
+| 1 | 2 | 3 | 4 | 5 | # Guess 5
+‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+_____________________
+| 1 | 2 | 3 | 4 | 5 | # Guess 6
+‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+
+⸨Correct⸩ [In Wrong Place] -Incorrect-
+
+"""
+
+import random
+
+words = ["other", "first", "click", "price", "state", "world", "music", "video", "order", "group", "under", "hotel", "store", "local", "phone", "board", "moral", "mayor", "prime", "watch", "power", "peace", "point", "limit", "pound", "thank", "think", "value", "valid", "wheel", "water", "solve", "force", "smart", "focus", "sugar", "radio", "voice", "throw", "magic", "major", "sight", "judge", "earth", "media", "green", "grass", "drama", "image", "dozen"] # 50 words
+
